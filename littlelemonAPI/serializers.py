62808c88842f4c2d96953c0ff4e89b848151514e
mutemip <paulmutemi2030@gmail.com>
2023-06-01 23:56:18 +0300

    concept: HYperlinkedmodelserializer

--- littlelemonAPI/serializers.py
+++ littlelemonAPI/serializers.py
@@ -5,20 +5,23 @@ from decimal import Decimal
 # handling nested fields in relationship serializer
 
 #method 1 - create serializer and include it in related model
-# class CategorySerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Category
-#         fields = ['id', 'slug', 'title']
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'slug', 'title']
 
-class MenuItemiSerializer(serializers.ModelSerializer):
+class MenuItemiSerializer(serializers.HyperlinkedModelSerializer):
     stock = serializers.IntegerField(source = 'inventory')
     price_after_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    # category = CategorySerializer()
+    # category = serializers.HyperlinkedRelatedField(
+    #     queryset = Category.objects.all(),
+    #     view_name= ('category-detail')
+    # )
     class Meta:
         model = MenuItem
         fields = ['id', 'title', 'price', 'stock', 'price_after_tax', 'category']
         # method 2 - instead of creation serializer, use depth=1 in meta class and all related fields of will be desplayed
-        depth = 1
+        # depth = 1
 
     def calculate_tax(self, product:MenuItem):
         return product.price * Decimal(1.16)
--- littlelemonAPI/views.py
+++ littlelemonAPI/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from .models import MenuItem, Category
-from .serializers import MenuItemiSerializer
+from .serializers import MenuItemiSerializer, CategorySerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+
 
 
 
@@ -11,8 +15,14 @@ class CategoryView(generics.ListCreateAPIView):
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.select_related('category').all()
     serializer_class = MenuItemiSerializer
+    
 
 class SingleItemModelSerializer(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemiSerializer
 
+@api_view()
+def category_detail(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    serialized_category = CategorySerializer(category)
+    return Response(serialized_category.data)
