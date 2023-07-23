4e784e94310459deb447e829745584e23292b37c
mutemip <paulmutemi2030@gmail.com>
2023-07-23 23:28:36 +0300

    CBV allows only GET request

--- littlelemonAPI/urls.py
+++ littlelemonAPI/urls.py
@@ -2,15 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # CBV using generics
+    # CBV urls using generics
     path('menu-item', views.MenuItemsView.as_view()), #CBV
+    path('menu-item/<int:pk>', views.SingleItemView.as_view()),
 
-    # CBV using ViewSets
-    path('menuitem', views.MenuItemViewSet.as_view()),
+    # CBV urls using ViewSets
+    path('menuitem', views.MenuItemViewSet.as_view({'get':'list'})),
+    path('menuitem/<int:pk>', views.MenuItemViewSet.as_view({'get':'retrieve'})),
 
+    # FBV urls
     path('menu-item1', views.menu_items), #FBV
     path('menu-item1/<int:pk>', views.single_item),
-    path('menu-item/<int:pk>', views.SingleItemView.as_view()),
     # path('category', views.CategoryView.as_view()),
     path('category/<int:pk>',views.category_detail, name='category-detail')
 ]
