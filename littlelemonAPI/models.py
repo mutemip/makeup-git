8d77932b4812e1b096cc0478794c4be025fecb37
mutemip <paulmutemi2030@gmail.com>
2023-09-11 18:18:42 +0300

    added cart, order, orderitems models

--- littlelemonAPI/models.py
+++ littlelemonAPI/models.py
@@ -13,6 +13,7 @@ class MenuItem(models.Model):
     title = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.SmallIntegerField()
+    featured = models.BooleanField(db_index=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
 
     def __str__(self):
@@ -22,3 +23,30 @@ class Rating(models.Model):
     menuitem_id = models.SmallIntegerField()
     rating =  models.SmallIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_length=6, decimal_places=2)
+    price = models.DecimalField(max_length=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('menuitem', 'user')
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    delivery_crew = models.ForeignKey(User, on_delete=models.CASCADE, related_name='delivery_crew', null=True)
+    status = models.BooleanField(db_index=True, default=0)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateField(db_index=True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(User, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('order', 'menuitem')
